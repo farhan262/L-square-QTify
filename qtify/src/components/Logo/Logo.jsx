964749dc500dcb946176cd724e182a29479c68a4
Farhan Qureshi <qureshifarhan26@gmail.com>
2023-03-17 18:06:07 +0530

    c1: navbar

---
+++ qtify/src/components/Logo/Logo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Logo() {
+  return (
+    <div>
+      <img src={require("../../assets/logo.png")} alt="logo" width={67} />
+    </div>
+  );
+}
+
+export default Logo;
