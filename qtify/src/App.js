0af9629715d4d865b313b958b977f28310fbc505
Farhan Qureshi <qureshifarhan26@gmail.com>
2023-03-18 19:11:46 +0530

    c3

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Card from "./components/card/Card";
+import Hero from "./components/hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 
 export default function App() {
   return (
     <>
       <Navbar />
+      <Hero />
+      <Card />
     </>
   );
 }
---
+++ qtify/src/components/card/Card.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./Card.module.css";
+function Card() {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.card}>
+        <img
+          src={require("../../assets/headphone.png")}
+          alt="headphone"
+          loading="lazy"
+        />
+        <div className={styles.banner}>
+          <div className={styles.pill}>
+            <p>1100 Follows</p>
+          </div>
+        </div>
+      </div>
+      <div className={styles.titlewrapper}>
+        <p>Music</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
